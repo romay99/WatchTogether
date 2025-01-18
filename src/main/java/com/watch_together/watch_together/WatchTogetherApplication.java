794222a87bcf99ecae2514f5d9c05f7b1460aef0
LeeSooBin <romaydev@naver.com>
2025-01-18 22:45:23 +0900

    first commit

---
+++ src/main/java/com/watch_together/watch_together/WatchTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.watch_together.watch_together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchTogetherApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WatchTogetherApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/watch_together/watch_together/WatchTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watch_together.watch_together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
