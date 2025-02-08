ac5fd3a8240d81994a8f5d4b178f570b8bc2c4b2
LeeSooBin <romaydev@naver.com>
2025-02-08 17:06:09 +0900

    refactor : KakaoPay 관련 DTO들 전부 카멜케이스로 변경

--- src/main/java/com/watchtogether/watchtogether/api/dto/KakaoPayReqeustDto.java
+++ src/main/java/com/watchtogether/watchtogether/api/dto/KakaoPayReqeustDto.java
@@ -1,5 +1,6 @@
 package com.watchtogether.watchtogether.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,16 +11,24 @@ import lombok.Setter;
 public class KakaoPayReqeustDto {
 
   private String cid;
-  private String partner_order_id; // 가맹점 주문번호, 최대 100자
-  private String partner_user_id; // 가맹점 회원 id, 최대 100자
+  @JsonProperty("partner_order_id")
+  private String partnerOrderId;// 가맹점 주문번호, 최대 100자
+  @JsonProperty("partner_user_id")
+  private String partnerUserId; // 가맹점 회원 id, 최대 100자
   // (실명, ID와 같은 개인정보가 포함되지 않도록 유의)
-  private String item_name; // 상품명 최대 100자
+  @JsonProperty("item_name")
+  private String itemName; // 상품명 최대 100자
   private int quantity; // 상품 수량
-  private int total_amount; // 상품 총액
-  private int tax_free_amount; // 상품 비과세 금액
-  private String approval_url; // 결제 성공시 url
-  private String cancel_url; // 결제 취소시 url
-  private String fail_url; // 결제 실패시 url
+  @JsonProperty("total_amount")
+  private int totalAmount; // 상품 총액
+  @JsonProperty("tax_free_amount")
+  private int taxFreeAmount; // 상품 비과세 금액
+  @JsonProperty("approval_url")
+  private String approvalUrl; // 결제 성공시 url
+  @JsonProperty("cancel_url")
+  private String cancelUrl; // 결제 취소시 url
+  @JsonProperty("fail_url")
+  private String failUrl; // 결제 실패시 url
 
   public static KakaoPayReqeustDto makeRequest(
       String cid,
@@ -28,16 +37,16 @@ public class KakaoPayReqeustDto {
       int amount) {
     return KakaoPayReqeustDto.builder()
         .cid(cid)
-        .partner_order_id(orderId)
-        .partner_user_id(memberId)
-        .item_name("포인트 충전")
+        .partnerOrderId(orderId)
+        .partnerUserId(memberId)
+        .itemName("포인트 충전")
         .quantity(1)
-        .total_amount(amount)
-        .tax_free_amount(0)
-        .approval_url(
+        .totalAmount(amount)
+        .taxFreeAmount(0)
+        .approvalUrl(
             "http://localhost:8080/kp/success?member_id=" + memberId + "&amount=" + amount)
-        .cancel_url("http://localhost:8080/kp/cancel")
-        .fail_url("http://localhost:8080/kp/fail")
+        .cancelUrl("http://localhost:8080/kp/cancel")
+        .failUrl("http://localhost:8080/kp/fail")
         .build();
   }
 }
--- src/main/java/com/watchtogether/watchtogether/api/dto/KakaoPayResponseDto.java
+++ src/main/java/com/watchtogether/watchtogether/api/dto/KakaoPayResponseDto.java
@@ -1,6 +1,7 @@
 package com.watchtogether.watchtogether.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,19 +16,24 @@ public class KakaoPayResponseDto {
   private String tid; // 결제 고유 번호, 20자
   // 요청한 클라이언트(Client)가 모바일 앱일 경우
   //카카오톡 결제 페이지 Redirect URL
-  private String next_redirect_app_url;
+  @JsonProperty("next_redirect_app_url")
+  private String nextRedirectAppUrl;
   //요청한 클라이언트가 모바일 웹일 경우
   //카카오톡 결제 페이지 Redirect URL
-  private String next_redirect_mobile_url;
+  @JsonProperty("next_redirect_mobile_url")
+  private String nextRedirectMobileUrl;
   //요청한 클라이언트가 PC 웹일 경우
   //카카오톡으로 결제 요청 메시지(TMS)를 보내기 위한
   // 사용자 정보 입력 화면 Redirect URL
-  private String next_redirect_pc_url;
+  @JsonProperty("next_redirect_pc_url")
+  private String nextRedirectPCUrl;
   //카카오페이 결제 화면으로 이동하는 Android 앱 스킴
-  private String android_app_scheme;
+  @JsonProperty("android_app_scheme")
+  private String androidAppScheme;
   //카카오페이 결제 화면으로 이동하는 iOS 앱 스킴
-  private String ios_app_scheme;
+  @JsonProperty("ios_app_scheme")
+  private String iosAppScheme;
   // 결제 준비 요청 시간
-  private LocalDateTime created_at;
-
+  @JsonProperty("created_at")
+  private LocalDateTime createdAt;
 }
